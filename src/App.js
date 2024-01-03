426465b4b73d6c8fde47d0003598ba66eb132e60
Y8a0 <halidouyacouba999@gmail.com>
2024-01-03 16:45:02 +0100

    App page edit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
+import { useState } from "react";
+import { data } from "./data";
+import logo from './images/logo.svg';
+import avatar from './images/image-avatar.png';
+import {FaCart} from 'react-icons';
+
+function Header() {
+  return (
+    <>
+      <div>
+        <img src={logo} alt=""/>
+
+        <nav>
+          <ul>
+            <li>Collections</li>
+            <li>Men</li>
+            <li>Women</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </div>
+
+      <div>
+        <ul>
+          <li><button><FaCart /></button></li>
+          <li><img src={avatar} alt="" /></li>
+        </ul>
+      </div>
+    </>
+  )
+}
 
 
 function App() {
+
+  const [products] = useState(data);
+
   return (
     <>
-      <h1>E commerce product page</h1>
+      <h1>{data.length}</h1>
     </>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const data = [
+    {
+        id: 1,
+        mainImage: 'image/image-product-1.jpg',
+        thumbnail: "image/image-product-1-thumbnail.jpg"
+    },
+    {
+        id: 2,
+        mainImage: 'image/image-product-2.jpg',
+        thumbnail: "image/image-product-2-thumbnail.jpg"
+    },
+    {
+        id: 3,
+        mainImage: 'image/image-product-3.jpg',
+        thumbnail: "image/image-product-3-thumbnail.jpg"
+    },
+    {
+        id: 4,
+        mainImage: 'image/image-product-4.jpg',
+        thumbnail: "image/image-product-4-thumbnail.jpg"
+    }
+]
